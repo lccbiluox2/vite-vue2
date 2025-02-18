2c29940862805eba0937bc9d46c058ebd02a0fde
541711153@qq.com <541711153@qq.com>
2025-02-18 22:08:45 +0800

    feat: 配置代理服务器

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,50 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import path from 'path';
 import { viteMockServe } from 'vite-plugin-mock';
 
-export default defineConfig(({ command }) => ({
-    plugins: [
-        vue(),
-        vueJsx(),
-        viteMockServe({
-            localEnabled: command === 'serve',
-            mockPath: 'mock/',
-            watchFiles: true,
-            injectFile: true,
-        } as any),
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src') // 确保路径别名指向 src 目录
-        }
-    },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                // 使用 @use 替代 @import
-                additionalData: `@use "@/styles/variables" as *;`
-            }
-        }
-    }
-}));
+export default defineConfig(({ command, mode }) => {
+    // 加载环境变量
+    const env = loadEnv(mode, process.cwd());
 
+    // 根据不同环境设置API前缀和目标地址
+    const apiPrefix = env.VITE_APP_API_URL_PREFIX || '/api';
+    const targetUrl = env.VITE_APP_API_URL || '';
+
+    return {
+        plugins: [
+            vue(),
+            vueJsx(),
+            viteMockServe({
+                localEnabled: command === 'serve',
+                mockPath: 'mock/',
+                watchFiles: true,
+                injectFile: true,
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src') // 确保路径别名指向 src 目录
+            }
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `@use "@/styles/variables" as *;`,
+                    // 如果使用的是最新版 Vite，请尝试改为 prependData
+                    // prependData: `@use "@/styles/variables" as *;`
+                }
+            }
+        },
+        server: {
+            proxy: {
+                [apiPrefix]: {
+                    target: targetUrl,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(new RegExp(`^${apiPrefix}`), ''),
+                },
+            },
+        },
+    };
+});
